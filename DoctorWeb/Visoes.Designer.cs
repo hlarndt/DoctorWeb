a4fa3fb0eb3f9d43e761bf4acacac885d49758f5
Heinrich Ludwig Arndt <heinricharndt@gmail.com>
2016-10-25 21:31:03 -0200

    Commit Initial

---
+++ DoctorWeb/Visoes.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace DoctorWeb
+{
+    partial class Visoes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExcelSelector = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Selecionar Arquivo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(522, 325);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(212, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(323, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Planilha ativa:";
+            // 
+            // Visoes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(547, 405);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Visoes";
+            this.Text = "DoctorWeb - Visões Configurador";
+            this.SizeChanged += new System.EventHandler(this.Visoes_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ExcelSelector;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DoctorWeb/Visoes.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoctorWeb
+{
+    public partial class Visoes : Form
+    {
+        public Visoes()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if(ExcelSelector.ShowDialog() == DialogResult.OK)
+            {
+                var _StringConexao = String.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 12.0 Xml;HDR=YES;ReadOnly=False';", ExcelSelector.FileName);
+                try
+                {
+                    var _olecon = new OleDbConnection(_StringConexao);
+                    _olecon.Open();
+                    var _oleCmd = new OleDbCommand();
+                    _oleCmd.Connection = _olecon;
+                    _oleCmd.CommandType = CommandType.Text;
+                    DataTable dt = _olecon.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+                    //Cria o objeto dataset para receber o conteúdo do arquivo Excel
+                    DataSet output = new DataSet();
+                    //Add each table name to the combo box
+                    if (dt != null && dt.Rows.Count > 0)
+                    {
+                        comboBox1.Items.Clear();
+                        foreach (DataRow row in dt.Rows)
+                        {
+                            comboBox1.Items.Add(row["TABLE_NAME"].ToString());
+                        }
+                    }
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        // obtem o noma da planilha corrente
+                        comboBox1.Text = row["TABLE_NAME"].ToString();
+                        string sheet = row["TABLE_NAME"].ToString();
+                        // obtem todos as linhas da planilha corrente
+                        OleDbCommand cmd = new OleDbCommand("SELECT * FROM [" + sheet + "]", _olecon);
+                        cmd.CommandType = CommandType.Text;
+                        // copia os dados da planilha para o datatable
+                        DataTable outputTable = new DataTable(sheet);
+                        output.Tables.Add(outputTable);
+                        new OleDbDataAdapter(cmd).Fill(outputTable);
+                    }
+                    dataGridView1.DataSource = output.Tables[0];
+                    dataGridView1.AutoGenerateColumns = true;
+                    dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCellsExceptHeader;
+                    // Neste momento, o conteúdo de todas as planilhas presentes no arquivo Excel foi copiado para os DataTables consolidados no Dataset "output".
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                var _StringConexao = String.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 12.0 Xml;HDR=YES;ReadOnly=False';", ExcelSelector.FileName);
+                var _olecon = new OleDbConnection(_StringConexao);
+                _olecon.Open();
+                var _oleCmd = new OleDbCommand();
+                _oleCmd.Connection = _olecon;
+                _oleCmd.CommandType = CommandType.Text;
+                DataTable dt = _olecon.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+                //Cria o objeto dataset para receber o conteúdo do arquivo Excel
+                DataSet output = new DataSet();
+                OleDbCommand cmd = new OleDbCommand("SELECT * FROM [" + comboBox1.Text + "]", _olecon);
+                string sheet = comboBox1.Text;
+                DataTable outputTable = new DataTable(sheet);
+                output.Tables.Add(outputTable);
+                new OleDbDataAdapter(cmd).Fill(outputTable);
+                cmd.CommandType = CommandType.Text;
+                // copia os dados da planilha para o datatable
+                dataGridView1.DataSource = output.Tables[0];
+                dataGridView1.AutoGenerateColumns = true;
+                dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCellsExceptHeader;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void Visoes_SizeChanged(object sender, EventArgs e)
+        {
+            dataGridView1.Width = Visoes.ActiveForm.Width - 50;
+            dataGridView1.Height = Visoes.ActiveForm.Height - 120;
+            comboBox1.Width = Visoes.ActiveForm.Width - 250;   
+        }
+    }
+}

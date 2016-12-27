2ab48d0186151b800b3c6ac4cc3301ee16b8b531
Heinrich Ludwig Arndt <heinricharndt@gmail.com>
2016-12-27 21:06:39 -0200

    Commit Crud Interface

---
+++ CrudInterface/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrudInterface.Startup))]
+namespace CrudInterface
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

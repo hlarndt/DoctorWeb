08cd8be5cb71e95ccba5e8c75b1c132bb129c108
Heinrich Ludwig Arndt <heinricharndt@gmail.com>
2017-03-21 22:58:03 -0300

    Commit Initial

--- CrudInterface/ficha.aspx
+++ CrudInterface/ficha.aspx
@@ -1,22 +1,26 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true"  %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ficha.aspx.cs" Inherits="CrudInterface.ficha" %>
+
 <!DOCTYPE html>
+
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
+<head runat="server">
     <meta charset="utf-8" />
-    <title>Doctor WEB - Ficha</title>
+    <meta name="viewport" content="width=device-width, initial-scale=0.8">
+    <title>Doctor WEB - Movimentos</title>
     <link href="/Content/bootstrap.css" rel="stylesheet"/>
     <link href="/Content/site.css" rel="stylesheet"/>
-    <script src="/Scripts/modernizr-2.6.2.js"></script>
-    <script src="/Scripts/jquery-1.10.2.js"></script>
-    <script src="/Scripts/bootstrap.js"></script>
-    <script src="/Scripts/respond.js"></script>
     <script type="application/json" id="__browserLink_initializationData">
         {"appName":"Firefox","requestId":"4d3c0c2834044ad18dc5f24ea407818b"}
     </script>
 </head>
 <body style="background-color: #FFFFCC;">
     <!--#include virtual="/menubar.aspx"-->
-    <div style="background-color: #FFFFCC; text-align: center; position: absolute; left: 50%; top: 50%; -webkit-transform: translate(-50%, -50%); transform: translate(-50%, -50%);">
+    <script src="/bundles/jquery?v=FVs3ACwOLIVInrAl5sdzR2jrCDmVOWFbZMY6g6Q0ulE1"></script>
+    <script src="/bundles/jqueryval?v=hEGG8cMxk9p0ncdRUOJ-CnKN7NezhnPnWIvn6REucZo1"></script>
+    <script src="/bundles/bootstrap?v=2Fz3B0iizV2NnnamQFrx-NbYJNTFeBJ2GM05SilbtQU1"></script>
+    <form id="form1" runat="server">
+    <div style="background-color: #FFFFCC;text-align:center;position:absolute;left:50%;top:50%;-webkit-transform: translate(-50%, -50%);transform: translate(-50%, -50%);">
     </div>
+    </form>
 </body>
 </html>
---
+++ CrudInterface/movimentos.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Diagnostics;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Web;
+using System.Web.UI;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Web.UI.WebControls;
+using System.Threading.Tasks;
+using System.IO;
+using System.Web.Mvc;
+using System.Text;
+
+namespace CrudInterface
+{
+    public partial class movimentos : System.Web.UI.Page
+    {
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if ((FileUpload1.PostedFile != null) && (FileUpload1.PostedFile.ContentLength > 0))
+            {
+                string fn = System.IO.Path.GetFileName(FileUpload1.PostedFile.FileName);
+                string SaveLocation = Server.MapPath("Data") + "\\" + fn;
+                try
+                {
+                    FileUpload1.PostedFile.SaveAs(SaveLocation);
+                    Label1.BackColor = System.Drawing.Color.Black;
+                    Label1.BorderColor = System.Drawing.Color.Black;
+                    Label1.BorderStyle = BorderStyle.Solid;
+                    Label1.Font.Bold = true;
+                    Label1.Font.Size = System.Web.UI.WebControls.FontUnit.Medium;
+                    Label1.ForeColor = System.Drawing.Color.Red;
+                    Label1.Height = 24;
+                    Label1.Text = "Arquivo " + FileUpload1.PostedFile.FileName.ToString() + " foi recebido.";
+                    if (File.Exists(SaveLocation))
+                    {
+                        var _StringConexao = String.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 12.0 Xml;HDR=YES;ReadOnly=False';", SaveLocation);
+                        var _olecon = new OleDbConnection(_StringConexao);
+                        _olecon.Open();
+                        var _oleCmd = new OleDbCommand();
+                        _oleCmd.Connection = _olecon;
+                        _oleCmd.CommandType = CommandType.Text;
+                        DataTable dt = _olecon.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+                        //Cria o objeto dataset para receber o conteúdo do arquivo Excel
+                        DataSet output = new DataSet();
+                        string firstplan = "";
+                        if (dt != null && dt.Rows.Count > 0)
+                        {
+                            foreach (DataRow row in dt.Rows)
+                            {
+                                Debug.WriteLine(row["TABLE_NAME"].ToString());
+                                if (firstplan.Length == 0)
+                                {
+                                    firstplan = row["TABLE_NAME"].ToString();
+                                }
+                            }
+                        }
+                        OleDbCommand cmd = new OleDbCommand("SELECT * FROM [" + firstplan + "]", _olecon);
+                        string sheet = firstplan;
+                        DataTable outputTable = new DataTable(sheet);
+                        output.Tables.Add(outputTable);
+                        new OleDbDataAdapter(cmd).Fill(outputTable);
+                        cmd.CommandType = CommandType.Text;
+                        GridView1.DataSource = output.Tables[0];
+                        GridView1.AutoGenerateColumns = true;
+                        GridView1.DataBind();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Label1.BackColor = System.Drawing.Color.Black;
+                    Label1.BorderColor = System.Drawing.Color.Black;
+                    Label1.BorderStyle = BorderStyle.Solid;
+                    Label1.Font.Bold = true;
+                    Label1.Font.Size = System.Web.UI.WebControls.FontUnit.Medium;
+                    Label1.ForeColor = System.Drawing.Color.Red;
+                    Label1.Height = 24;
+                    Label1.Text = "Erro: " + ex.Message;
+                }
+            }
+            else
+            {
+                Label1.BackColor = System.Drawing.Color.Black;
+                Label1.BorderColor = System.Drawing.Color.Black;
+                Label1.BorderStyle = BorderStyle.Solid;
+                Label1.Font.Bold = true;
+                Label1.Font.Size = System.Web.UI.WebControls.FontUnit.Medium;
+                Label1.ForeColor = System.Drawing.Color.Red;
+                Label1.Height = 24;
+                Label1.Text = "Por favor, selecione um arquivo.";
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
